8f125cb0478949cfb20775f2829c2c3fc23291bd
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2026-01-01 02:28:19 -0600

    Update Vite configuration and GitHub Actions workflow
    - Added console log for mode in Vite configuration for better debugging.
    - Adjusted base path logic for deployment based on environment.
    - Set appType to "spa" in Vite configuration.
    - Removed unnecessary submodule initialization step from GitHub Actions workflow to streamline deployment process.

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  console.log("mode:", mode);
   const isDev = mode === "development";
   // When deploying to GitHub Pages, use the repo name as the base path
-  // const base = isDev ? "/" : "/zeckendorf-webapp/";
-  const base = "/";
+  const base = isDev ? "/" : "/zeckendorf-webapp/";
 
   return {
     server: {
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
       port: 8080,
     },
     base,
+    appType: "spa",
     plugins: [
       wasm(),
       react(),
@@ -35,60 +36,60 @@ export default defineConfig(({ mode }) => {
           orientation: "portrait",
           scope: base,
           start_url: base,
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2,wasm}"],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+          icons: [
+            {
+              src: "/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2,wasm}"],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "google-fonts-cache",
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "gstatic-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+            {
+              urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "gstatic-fonts-cache",
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-        ],
-      },
-    }),
-  ].filter(Boolean),
+          ],
+        },
+      }),
+    ].filter(Boolean),
     build: {
       target: "es2022", // Supports top-level await for the generated wasm code
     },
